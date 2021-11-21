fdcd13fb74b4e4f0fe1cf42704f6131fc4bcc760
joelhernandezvasquez <jhernandez98@outlook.es>
2021-11-21 14:34:53 -0500

    change the api env variable

--- pages/api/main.js
+++ pages/api/main.js
@@ -4,7 +4,7 @@ import {API_URL} from '../../config/index'
 // Function to make a POST request to the endpoint /api/guestbook
 export const createPost = async data => {
   try {
-    const response = await axios.post(`${API_URL}/api/guestbook`, { data })
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/guestbook`, { data })
     return {
       success: true,
       data: response.status
@@ -21,7 +21,7 @@ export const createPost = async data => {
 // Function to make a GET request to the endpoint /guestbook
 export const getPosts = async () => {
   try {
-    const response = await fetch(`${API_URL}/guestbook`)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/guestbook`)
     const result = await response.json()
 
     return {
